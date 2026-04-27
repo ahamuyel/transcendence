b6fe522b439306ce912776124a975d292421a8f3
ahamuyel <ahamuyel@cur10us.myguest.virtualbox.org>
2026-04-27 14:48:09 +0100

    compose done & makefile

--- api/index.js
+++ api/index.js
@@ -4,8 +4,16 @@ const app = express();
 const PORT = 4000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from API" });
+  res.json({
+    message: "Hello from Cur10usX in a container!",
+    service: "Hello-node",
+    pod: process.env.POD_NAME || "unknown",
+    time: new Date().toISOString(),
+  });
 });
+
+app.get('/readyz', (req, res)=> res.status(200).send('ready'))
+app.get('/healthz', (req, res)=> res.status(200).send('ok'))
 
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
